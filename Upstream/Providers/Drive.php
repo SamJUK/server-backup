f5335e2e8421393ec3988f9a4671ce41b4ae1c17
Sam James <sam@samdjames.uk>
2019-02-24 18:23:37 +0000

    Added drive files

---
+++ Upstream/Providers/Drive.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Upstream\Providers;
+
+use App;
+use Upstream\UpstreamBase;
+use Upstream\Exceptions\FolderMissingException;
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Exception\ClientException as GuzzleException;
+
+
+class Drive extends UpstreamBase
+{
+
+}
---
+++ Upstream/Providers/Drive/Authentication.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Upstream\Providers\Drive;
+
+use Upstream\AuthenticationInterface;
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Exception\ClientException as GuzzleException;
+
+class Authentication implements AuthenticationInterface
+{
+
+
+}
